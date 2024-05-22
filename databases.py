dbf94e93e99a9813a4801cd0aee03f0960c6673e
AndreiAnatolievich <152917682+AndreiAnatolievich@users.noreply.github.com>
2024-05-22 10:00:40 +0400

    Update databases.py

---
+++ databases.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, filedialog, messagebox, simpledialog
+
+from numpy import asarray, savetxt
+
+import matplotlib.pyplot as plt
+
+import matplotlib.image as mpimg
+
+root = Tk() # open the dialog box
+
