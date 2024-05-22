b573888897ddbecc4db85babf584cc88ac4d0745
AndreiAnatolievich <152917682+AndreiAnatolievich@users.noreply.github.com>
2024-05-22 10:04:36 +0400

    Update databases.py

--- databases.py
+++ databases.py
@@ -8,3 +8,20 @@ import matplotlib.image as mpimg
 
 root = Tk() # open the dialog box
 root.withdraw() # first hide the root window
+# open the dialog
+filein = filedialog.askopenfilename(
+title = "Select image to digitize",
+filetypes = (
+("png files","*.png"),
+("jpeg files","*.jpg"))
+)
+
+# show the image
+img = mpimg.imread(filein)
+_, ax = plt.subplots()
+ax.imshow(img)
+
+# ax.axis('off') # clear x-axis and y-axis
+# get reference length in x direction
+
+def getReferenceLength(index):
