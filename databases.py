b89e12f8fdfd81515dca5c3201d5f1b490f73335
AndreiAnatolievich <152917682+AndreiAnatolievich@users.noreply.github.com>
2024-05-22 10:15:38 +0400

    Update databases.py

--- databases.py
+++ databases.py
@@ -54,3 +54,21 @@ reply = messagebox.askyesno("Information",
 You selected from {:4.4f} to {:4.4f} in {:s}. Is this correct?".format(data1,data2,direction)
 )
 PiForPo=(coord2[0][index]-coord1[0][index])/(data2-data1)
+return [PiForPo,data1,coord1]
+xfactor = getReferenceLength(0)
+yfactor = getReferenceLength(1)
+
+# digitize curves until stoped by the user
+messagebox.showinfo("Digitize curve",
+"Please digitize the curve. The first point is the origin." +
+"Left click: select point; Right click: undo; Middle click: finish"
+)
+
+# get the curve points
+data = plt.ginput(-1,timeout=0,show_clicks=True)
+data = asarray(data)
+ax.plot(data[:,0],data[:,1],'g','linewidth',1.5)
+plt.draw()
+
+# convert the curve points from pixels to coordinates
+
